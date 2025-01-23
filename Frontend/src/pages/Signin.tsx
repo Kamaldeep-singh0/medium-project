d480c13c83b5bae77a7c61632816ba1cfcc094c6
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-01-23 22:16:56 +0530

    frontend

---
+++ Frontend/src/pages/Signin.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Signin() {
+  return (
+    <div className='flex'>
+    <div className='w-3/6 h-screen   flex flex-col items-center justify-center p-5'>
+      <div className='bg-white w-2/3 h-5/6 flex flex-col items-center p-5' >
+          <h1 className='text-4xl font-bold'>Login your Account</h1>
+          <h3 className='text-lg text-gray-600 my-2'>New to Medium? <Link to="/signup" className='underline'>SignUp</Link></h3>
+
+          <h1 className='text-xl font-medium my-2 w-full '>Email</h1>
+          <input  type="text" className=' mb-2 mx-5 px-4 h-10 w-full border border-gray-300 rounded' placeholder='JohnDoe@gmail.com'/>
+
+          <h1 className='text-xl font-medium my-2 w-full '>Password</h1>
+          <input  type="text" className=' mb-2 mx-5 px-4 h-10 w-full border border-gray-300 rounded' placeholder='Secret'/>
+
+          <button className='my-5 w-full h-12 bg-black rounded-md text-white text-xl font-semibold'>Sign in</button>
+
+      </div>
+    </div>
+    <div className='w-3/6 h-screen bg-slate-100  flex flex-col items-center justify-center px-20'>
+      <h1 className='text-3xl font-bold w-full'>"The customer service I received was exceptional. The support team went above and beyond to address my concerns."</h1>
+      <h2 className='w-full pt-3 text-lg font-semibold'>Jules Winifield</h2>
+      <h3 className='w-full text-sm font-semibold text-gray-500'>CEO, Acme Inc</h3>
+    </div>
+  </div>
+  )
+}
+
+export default Signin
