92faef84d5345ec7a46f6810465f5f0437678bce
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-01-25 01:51:13 +0530

    Creating- frontend

--- Frontend/src/components/BlogCard.tsx
+++ Frontend/src/components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface BlogCardProps{
     id: number,
@@ -9,12 +10,33 @@ interface BlogCardProps{
 
 }
 
-function BlogCard({id,authorName,tile,content,publishedDate}: BlogCardProps) {
+function BlogCard({id,authorName,title,content,publishedDate}: BlogCardProps) {
   return (
-    <div>
-      
+    <Link to={`/blog/${id}`}>
+    <div className='py-8 border-b-2'>
+      <div className='flex items-center'>
+         <Avatar name={authorName} />
+        <div className='ml-2 text-base '>{authorName}</div>
+        <div  className='ml-2 text-sm text-gray-500 '>{publishedDate}</div>
+      </div>
+      <div className='text-xl font-bold mt-2'>{title}</div>
+      <div className='font-serif font-thin'>{content.slice(0,200)+"..."}</div>
+      <div className="text-slate-500 text-sm font-thin pt-4">{`${Math.ceil(content.length/100)} minute(s) read`}</div>
     </div>
+    
+    
+    </Link>
   )
+}
+
+export function Avatar({name,size = "small"}:{name:string, size?:"small"|"big"}){
+    return(
+        <div className='inline-flex items-center justify-center size-[28px] rounded-full bg-yellow-500 font-semibold text-black leading-none'>
+           <span className={`${size === "small" ? "text-xl" : "text-md"} `}>
+        {name[0]}
+    </span>
+        </div>
+    )
 }
 
 export default BlogCard
--- Frontend/src/pages/Blog.tsx
+++ Frontend/src/pages/Blog.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import BlogCard from '../components/BlogCard'
 
 function Blog() {
   return (
-    <div>
-      
+    <div className="flex justify-center h-screen">
+    <div className='flex flex-col w-2/3 justify-center '>
+      <BlogCard id={123} authorName="Kamaldeep" title="Hello t is a long established fact that a reader will be distracted my dear friends  iam here"
+        content="Hello my dear friends  iam here Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." publishedDate="17/01/2005"/>
+     <BlogCard id={123} authorName="Kamaldeep" title="Hello t is a long established fact that a reader will be distracted my dear friends  iam here"
+        content="Hello my dear friends  iam here Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." publishedDate="17/01/2005"/>
+         <BlogCard id={123} authorName="Kamaldeep" title="Hello t is a long established fact that a reader will be distracted my dear friends  iam here"
+        content="Hello my dear friends  iam here Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." publishedDate="17/01/2005"/>
+    </div>
     </div>
   )
 }
